071cfed366d4a75075e40dc1e62758edb656c249
Eunhak Lee <admin@return0927.xyz>
2020-02-02 21:26:03 +0900

    /posts 에 limit, author_id 추가

--- backend/routes/api_v1/posts.py
+++ backend/routes/api_v1/posts.py
@@ -1,4 +1,4 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 # from flask_sqlalchemy import
 
 from ... import db, Post
@@ -11,7 +11,18 @@ from typing import List
 class Posts(Resource):
     def get(self):
         try:
-            posts: List[Post] = Post.query.all()
+            parser = reqparse.RequestParser()
+            parser.add_argument('limit', type=int)
+            parser.add_argument('author_id', type=int)
+
+            args = parser.parse_args()
+            author_id = args.get("author_id", 0)
+            limit = args.get("limit", 100)
+
+            if author_id:
+                posts: List[Post] = Post.query.filter(Post.author_id == author_id).limit(limit).all()
+            else:
+                posts: List[Post] = Post.query.limit(limit).all()
 
             return {
                 "error": False,
