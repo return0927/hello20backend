2542cdbd9f1bbf4afdc67dc0a6cd3d8707b19cfe
Eunhak Lee <admin@return0927.xyz>
2020-02-02 17:56:25 +0900

    Posts: 구조 개발 및 예시 반응 추가

--- backend/routes/api_v1/__init__.py
+++ backend/routes/api_v1/__init__.py
@@ -1,7 +1,9 @@
 from flask import Blueprint
+from flask_restful import Api
 
 
 api_v1 = Blueprint('api_v1', __name__, template_folder="templates")
+api = Api(api_v1)
 
 ####################
 # Error handlers
@@ -12,3 +14,6 @@ api_v1.register_error_handler(404, handle_404)
 ####################
 # Api Routes
 from .ping import api_ping
+from .posts import Posts
+
+api.add_resource(Posts, '/posts')
